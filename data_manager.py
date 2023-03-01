27199dc9f77f79cd7ff8314b34aa46bbb1535351
Tariehk <tag3win@gmail.com>
2023-03-01 10:49:27 -0500

    update for the morning of march 1st

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,8 @@
 import requests
 from pprint import pprint
 
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/35eee830991c22f8d195d5bdad73dfee/flightDeals/prices"
+
 
 class DataManager:
 
@@ -8,17 +10,25 @@ class DataManager:
         self.destination_data = {}
 
     def get_destination_data(self):
-    # Use the Sheety API to Get all the data in taht sheet and print it out
-
-        GOOGLE_SHEET_NAME = "prices"
-        sheet_endpoint = "https://api.sheety.co/35eee830991c22f8d195d5bdad73dfee/flightDeals/prices"
-        response = requests.get(sheet_endpoint)
+        # 2. Use the Sheety API to Get all the data in taht sheet and print it out
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
 
-    def update_iata_codes(self):
-        """Update the Google Sheet with teh IATA Codes"""
+    # 6 In the DataManager Class make a put request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes (Do this using code.).
 
-
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+        response = requests.put(
+            url=f"{SHEETY_PRICES_ENDPOINT}/{city['id]}",
+            json=new_data
+        )
+        print(response.text)
 
