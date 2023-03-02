e8b5d18ea643ead1021b130b9fba3cc218d74ed7
Tariehk <tag3win@gmail.com>
2023-03-02 09:28:38 -0500

    update for march 2nd morning

--- flight_search.py
+++ flight_search.py
@@ -36,4 +36,33 @@ class FlightSearch:
         # return the IATA code
         return iata_code
 
+    def check_fights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+
+        }
+
+        response = requests.get(
+            url=f"{TEQUILA_API_KEY}/v2/search",
+            headers=headers,
+            params=query
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+
 
--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import requests
 
 from data_manager import DataManager
 from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
 # Start the class
 data_manager = DataManager()
 
 # Grab the data from the spreadsheet
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "LOU"
 
 
 #  5. In main.py check if sheet_data contains any values for the "iataCode" key.
@@ -27,4 +31,14 @@ if sheet_data[0]["iataCode"] == "":
     data_manager.get_destination_data = sheet_data
     data_manager.update_destination_codes()
 
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6*30))
 
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        tomorrow,
+        six_month_from_today)
+
+    )
